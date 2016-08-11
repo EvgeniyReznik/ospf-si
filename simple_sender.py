779b4856a19577180d16bd61dbeed8a497d220e7
Evgeniy Reznik <ujenreznik@gmail.com>
2016-08-11 06:58:29 -0700

    added icmp packet

--- simple_sender.py
+++ simple_sender.py
@@ -10,7 +10,7 @@ PROTOCOL_UDP    = 0x11
 PROTOCOL_ICMP   = 0x01
 PROTOCOL_IGMP   = 0x02
 
-SEND_INTERFACE_NAME = "veth0"
+SEND_INTERFACE_NAME = "ens33"
 PORT_NUMBER = 1
 
 rawSocketSend = socket.socket(socket.PF_PACKET, socket.SOCK_RAW, socket.IPPROTO_RAW)
@@ -24,13 +24,21 @@ hello_packet_310 = ''
 for c in ospf_hello_packets.OSPF_HELLO_PACKET_310.split(' '):
     hello_packet_310 += chr(int(c, 16))
 
+ipv4_ping_request_packet = ''
+for c in ospf_hello_packets.IPV4_PING_REQUEST_PACKET.split(' '):
+    ipv4_ping_request_packet += chr(int(c, 16))
+
+
 
 for i in range(0, 1000000, 1):
+    rawSocketSend.send(ipv4_ping_request_packet)
     for j in range(0, 10, 1):
         rawSocketSend.send(hello_packet_130)
         # print "Sending packet: ", i
-        sleep(0.100)
+        sleep(0.125)
+    rawSocketSend.send(ipv4_ping_request_packet)
     for j in range(0, 10, 1):
         rawSocketSend.send(hello_packet_310)
         # print "Sending packet: ", j
-        sleep(0.100)
+        sleep(0.125)
+    rawSocketSend.send(ipv4_ping_request_packet)
